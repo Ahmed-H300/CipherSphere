7ce852caea2d4cd96fd556d5f83e2ec067a09190
Ahmed-H300 <ahmed300hany@gmail.com>
2023-04-11 02:01:16 +0200

    config

---
+++ config/__init__.py
@@ -0,0 +1,4 @@
+# Path: config\__init__.py
+
+from .constants import *
+
---
+++ config/constants.py
@@ -0,0 +1,27 @@
+# This file contains the information about the server address and the port and all other constants related to the chat.
+import socket
+
+SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
+SERVER_PORT = 5050
+HEADER = 64
+FORMAT = 'utf-8'
+GROUP_SIZE = 5
+DISCONNECT_MESSAGE = "disconnect"
+NEWLINE_MESSAGE = "NEW_LINE"
+EXCHANGE_MESSAGE = "EXCHANGE_MESSAGE"
+NUMBER_BITS = 70
+
+MODE_MESSAGE = '''
+*    Welcome to Cipher Sphere!                  *
+*    Where your Texts are safe and encrypted    *
+*    Enter the mode you want to use:            *
+*    1. Establish Connection                    *
+*    2. Connect                                 *
+*    3. Exit                                    *
+'''
+
+EXIT_MESSAGE = '''
+*    Thank you for using Cipher Sphere!         *
+*    Have a nice day!                           *
+'''
+
